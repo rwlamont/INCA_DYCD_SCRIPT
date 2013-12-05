7dcc2f399dfcd4bd17a3a1af3bc0c63a0c776488
rwlamont <rwlamont@gmail.com>
2013-12-06 12:58:54 +1300

    slight update, interleaves output
    interleaves output between the new data for inflow 1 and 2 and the
    existing data for inflow three (groundwater)

--- Okaro Inca Script/Program.cs
+++ Okaro Inca Script/Program.cs
@@ -10,7 +10,7 @@ namespace Okaro_Inca_Script
 {
     class IncaStrings
     {
-        public string Flow {get;set;}
+        public string Flow { get; set; }
         public string NO3 { get; set; }
         public string NH4 { get; set; }
         public string Vol { get; set; }
@@ -28,7 +28,7 @@ namespace Okaro_Inca_Script
             string batFile = File.ReadLines("setup.txt").Skip(7).Take(1).First();
             string[] splitFile1 = file1.Split(',');
             string[] splitFile2 = file2.Split(',');
-            string[] stream1Lines = new string [1460];
+            string[] stream1Lines = new string[1460];
             string[] stream2Lines = new string[1460];
             int skip = int.Parse(splitFile1[1]);// How many lines to skip
             // Reads in stream 1 data
@@ -44,13 +44,13 @@ namespace Okaro_Inca_Script
                 stream2Lines[i] = File.ReadLines(splitFile2[0]).Skip(skip).Take(1).First();
                 skip++;
             }
-            
-            //Puts the data read in into incastring objects and then finally into a list
-             for (int i = 0; i < 1460; i++)
-              {
-                 int itemCount = 0;
-                 IncaStrings newIncaStream1 = new IncaStrings();
-                 IncaStrings newIncaStream2 = new IncaStrings();
+
+            //Puts the data read in into inca string objects and then finally into a list
+            for (int i = 0; i < 1460; i++)
+            {
+                int itemCount = 0;
+                IncaStrings newIncaStream1 = new IncaStrings();
+                IncaStrings newIncaStream2 = new IncaStrings();
                 string[] splitStream1 = stream1Lines[i].Trim().Split(' ');
                 string[] splitStream2 = stream2Lines[i].Trim().Split(' ');
                 for (int j = 0; j < splitStream1.Count(); j++)
@@ -102,51 +102,79 @@ namespace Okaro_Inca_Script
                         itemCount++;
                     }
                 }
-                 data.Add(newIncaStream1);
-                 data.Add(newIncaStream2);
-             }
-            
+                data.Add(newIncaStream1);
+                data.Add(newIncaStream2);
+            }
+
             //Writes data to the new file
-             string seperator = "\t";
-             StreamWriter writer = new StreamWriter("output.txt");
-             StreamReader reader = new StreamReader(inflowFile);
-             writer.WriteLine(reader.ReadLine());
-             writer.WriteLine(reader.ReadLine());
-             writer.WriteLine(reader.ReadLine());
-             writer.WriteLine(reader.ReadLine());
-             writer.WriteLine(reader.ReadLine());
-             writer.WriteLine(reader.ReadLine());
-             int counter = 6;
-             foreach (IncaStrings inca in data)
-             {
-                 if (counter > 756) break;
-                 counter++;
+            string seperator = "\t";
+            StreamWriter writer = new StreamWriter("output.txt");
+            StreamReader reader = new StreamReader(inflowFile);
 
-                 string line = reader.ReadLine();
-                 string[] splitLine = line.Split('\t');
-                 writer.WriteLine(splitLine[0] + seperator + splitLine[1] + seperator + inca.Vol + seperator + splitLine[3] + seperator + splitLine[4] + seperator + splitLine[5] + seperator + inca.NO3 + seperator + inca.NH4 + seperator + splitLine[8] + seperator + splitLine[9] + seperator +splitLine[10] + seperator +splitLine[11] + seperator +splitLine[12] + seperator + splitLine[13]); // needs to go to 13
-             }
-             writer.Write(reader.ReadToEnd());
+            //Sets up the Meta details at the top of the file
+            writer.WriteLine(reader.ReadLine());
+            writer.WriteLine(reader.ReadLine());
+            writer.WriteLine(reader.ReadLine());
+            writer.WriteLine(reader.ReadLine());
+            writer.WriteLine(reader.ReadLine());
+            writer.WriteLine(reader.ReadLine());
+            int counter = 6;
+            // Does the first 755 lines of the file that have only the two stream inflows
+            foreach (IncaStrings inca in data)
+            {
+                if (counter > 755) break;
+                counter++;
 
+                string line = reader.ReadLine();
+                string[] splitLine = line.Split('\t');
+                writer.WriteLine(splitLine[0] + seperator + splitLine[1] + seperator + inca.Vol + seperator + splitLine[3] + seperator + splitLine[4] + seperator + splitLine[5] + seperator + inca.NO3 + seperator + inca.NH4 + seperator + splitLine[8] + seperator + splitLine[9] + seperator + splitLine[10] + seperator + splitLine[11] + seperator + splitLine[12] + seperator + splitLine[13]); // needs to go to 13
+            }
+
+            //Adds in the data once the inflow file changes from two inflows to three
+            for (int z = counter; z <= data.Count(); )
+            {
+
+                string line = reader.ReadLine();
+                if (line == null) break;
+                string[] splitLine = line.Split('\t');
+                writer.WriteLine(splitLine[0] + seperator + splitLine[1] + seperator + data[z].Vol + seperator + splitLine[3] + seperator + splitLine[4] + seperator + splitLine[5] + seperator + data[z].NO3 + seperator + data[z].NH4 + seperator + splitLine[8] + seperator + splitLine[9] + seperator + splitLine[10] + seperator + splitLine[11] + seperator + splitLine[12] + seperator + splitLine[13]); // needs to go to 13
+                z++;
+                line = reader.ReadLine();
+                splitLine = line.Split('\t');
+                writer.WriteLine(splitLine[0] + seperator + splitLine[1] + seperator + data[z].Vol + seperator + splitLine[3] + seperator + splitLine[4] + seperator + splitLine[5] + seperator + data[z].NO3 + seperator + data[z].NH4 + seperator + splitLine[8] + seperator + splitLine[9] + seperator + splitLine[10] + seperator + splitLine[11] + seperator + splitLine[12] + seperator + splitLine[13]); // needs to go to 13
+                line = reader.ReadLine();
+                writer.WriteLine(line);
+                z++;
+            }
             //Clean up
-             writer.Close();
-             reader.Close();
-             Console.WriteLine("Copying File...");
-            //TODO: fix up the copy files process
-             System.IO.File.Copy("output.txt", inflowFile, true);
-             // Start the child process.
-             Process p = new Process();
-             
-             Console.WriteLine("Starting Model Run...");
-           p.StartInfo.UseShellExecute = false;
-             p.StartInfo.RedirectStandardOutput = true;
-             p.StartInfo.FileName = batFile;
-             p.Start();
-             Console.WriteLine(p.StandardOutput.ReadToEnd());
-             p.WaitForExit(); 
-             Console.WriteLine("done");
-             Console.ReadLine();
+            writer.Close();
+            reader.Close();
+            Console.WriteLine("Copying File...");
+            System.IO.File.Copy("output.txt", inflowFile, true);
+            // Start the child process.
+            Process p = new Process();
+
+            Console.WriteLine("Starting Model Run...");
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = batFile;
+            p.OutputDataReceived += p_OutputDataReceived;
+
+            p.Start();
+            p.BeginOutputReadLine();
+            p.WaitForExit();
+            p.Close();
+            Console.WriteLine("Done");
+            Console.ReadLine();
 
         }
+
+        public static void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
     }
+
+
+
 }
